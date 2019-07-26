e1ebf8d4e53831870f587dee84e7a5ab37dbc956
Noble Obioma <nobioma1@gmail.com>
2019-07-26 09:22:41 +0100

    Create tests for login and register routes

---
+++ config/routes.test.js
@@ -0,0 +1,56 @@
+const request = require('supertest');
+const server = require('../api/server');
+const db = require('../database/dbConfig');
+
+beforeEach(async () => {
+  await db('users').truncate();
+});
+
+const user = {
+  username: 'testingja',
+  password: 'test',
+};
+
+describe('Routes', () => {
+  describe('[POST] /api/register', () => {
+    it('should return 201 on success', async () => {
+      await request(server)
+        .post('/api/register')
+        .send(user)
+        .expect(201);
+    });
+
+    it('should return userId and username on success', async () => {
+      const res = await request(server)
+        .post('/api/register')
+        .send(user);
+      expect(res.body).toHaveProperty('id', 1);
+      expect(res.body).toHaveProperty('username', user.username);
+    });
+  });
+
+  describe('[POST] /api/login', () => {
+    it('should return 200 on success', async () => { 
+      await request(server)
+        .post('/api/register')
+        .send(user)
+
+      await request(server)
+        .post('/api/login')
+        .send(user)
+        .expect(200);
+    });
+
+    it('should return message and token on success', async () => {
+      await request(server)
+        .post('/api/register')
+        .send(user)
+
+      const res = await request(server)
+        .post('/api/login')
+        .send(user);
+      expect(res.body).toHaveProperty('message', `Welcome ${user.username}!`);
+      expect(res.body).toHaveProperty('token');
+    });
+  });
+});
